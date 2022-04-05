35ee5f01395b4b040cd89533159023d866420175
Zexion16 <84300173+Zexion16@users.noreply.github.com>
2022-04-05 16:58:32 +0300

    Add files via upload

---
+++ app/app/Program.cs
@@ -0,0 +1,44 @@
+﻿
+Console.WriteLine("how many students");
+int num = Console.Read();
+float grade;
+gradeBook[] stuGrades = new gradeBook[num];
+gradeBook stuGrade = new gradeBook(null, 0);
+string name;
+
+for (int i = 0; i <= num; i++)
+{
+    Console.WriteLine($"enter the {i} student name");
+    name = Console.ReadLine();
+
+    Console.WriteLine($"enter the {i} student grade");
+    grade = Console.Read();
+
+    stuGrade = new gradeBook(name, grade);
+    stuGrades[i] = stuGrade;
+}
+stuGrade.avg(stuGrades);
+string text = Console.ReadLine();
+
+Console.WriteLine($"Your name is {text}");
+
+class gradeBook
+{
+    string name;
+    float grade;
+    public gradeBook(string name, float grade)
+    {
+        this.name = name;
+        this.grade = grade;
+    }
+    public float avg(gradeBook[] grade)
+    {
+        float x = 0;
+        for (int i = 0; i < grade.Length; i++)
+        {
+            grade[i].grade += x;
+        }
+
+        return x / grade.Length;
+    }
+}
